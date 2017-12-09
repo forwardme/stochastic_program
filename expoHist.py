d6052b02e159e449b13a40593058e7258d0d4adc
GaoYuan <forwardme@126.com>
2017-12-09 19:09:57 +0800

    add expoHist

---
+++ expoHist.py
@@ -0,0 +1,19 @@
+﻿import random, pylab
+def successfulStarts(eventProb, numTrials):
+    triesBeforeSuccess = []
+    for t in range(numTrials):
+        consecFailures = 0
+        while random.random() > eventProb:
+            consecFailures += 1
+        triesBeforeSuccess.append(consecFailures)
+    return triesBeforeSuccess
+    
+random.seed(0)
+probOfSuccess = 0.5
+numTrials = 5000
+distribution = successfulStarts(probOfSuccess, numTrials)
+pylab.hist(distribution, bins = 14)
+pylab.xlabel('Tries Before Success')
+pylab.ylabel('Number of Occurrences Out of ' + str(numTrials))
+pylab.title('Probability of Starting Each Try ' + str(probOfSuccess))
+pylab.show()
