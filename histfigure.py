74f0b2d2342390215fce4327c90d03148989000f
GaoYuan <forwardme@126.com>
2017-12-09 13:15:48 +0800

    add hist figure , sd

---
+++ histfigure.py
@@ -0,0 +1,8 @@
+﻿import random, pylab
+vals = [1,200]
+for i in range(1000):
+    num1 = random.choice(range(1,100))
+    num2 = random.choice(range(1,100))
+    vals.append(num1 + num2)
+pylab.hist(vals, bins = 50)
+pylab.show()
---
+++ stdDeviation.py
@@ -0,0 +1,8 @@
+﻿def stdDev(X):
+    """假设X是一个数字列表
+       返回X的标准差"""
+    mean = float(sum(X))/len(X)
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    return (tot/len(X))**0.5
